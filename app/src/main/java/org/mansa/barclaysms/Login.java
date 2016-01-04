ec2af5f2d6a7bf77cf781cf65b69a81681a7344b
martnga <mtnjoroge@gmail.com>
2016-01-04 12:56:08 +0300

    Initial Import

---
+++ app/src/main/java/org/mansa/barclaysms/Login.java
@@ -0,0 +1,16 @@
+package org.mansa.barclaysms;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by mansa on 1/4/16.
+ */
+public class Login extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
